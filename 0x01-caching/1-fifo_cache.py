20ff4bc6b5885e849e9324a5360b5135d4d0449b
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-07-25 17:08:55 +0400

    FIFO cach

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""FIFO caching"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Basic caching class"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """assing item, key to cache_data"""
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard = self.queue.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in cache_data"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
